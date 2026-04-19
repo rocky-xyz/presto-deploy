03b5444750dbf562c650bee71626731b078e2b59
FoLAWy <ljp2219819716@gmail.com>
2026-04-19 15:35:00 +1000

    Ass4:add vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,19 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setup.js',
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
+
   },
 })
